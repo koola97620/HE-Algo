fc36090b77c247012b3dc003ae38f04033b4cf84
Jaeyong <koola97620@nate.com>
2016-08-29 23:29:42 +0900

    Death 추

--- HE_Algo/src/input_output/Death.java
+++ HE_Algo/src/input_output/Death.java
@@ -4,27 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ToggleString {
+public class Death {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
         
-		String line;
 		try {
 			line = br.readLine();
-			char[] ch = line.toCharArray();
 			
-			for(int i=0 ; i < ch.length ; i++) {
-				if(Character.isLowerCase(ch[i])==true) {
-					ch[i] = Character.toUpperCase(ch[i]);
-					
-				} else if (Character.isUpperCase(ch[i]) == true) {
-					ch[i] = Character.toLowerCase(ch[i]);
-				}
-				System.out.print(ch[i]);
-			}
+			int N = Integer.parseInt(line);
+			int[] A = new int[N];
+	        for (int i = 0; i < N; i++) {
+	        	A[i] = Integer.parseInt(br.readLine());
+	        }
+	        for(int i =0 ; i <N ; i++) {
+	        	System.out.println(A[i]-1);
+	        }
 	        
 			
 			
@@ -32,8 +29,9 @@ public class ToggleString {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+        
+        
+
 	}
 
 }
